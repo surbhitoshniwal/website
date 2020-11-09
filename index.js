e9091a43b0b32a39d943934622335f89f20d2dbc
surbhitoshniwal <74205247+surbhitoshniwal@users.noreply.github.com>
2020-11-09 23:24:07 +0530

    uploaded personal website

---
+++ index.js
@@ -0,0 +1,10 @@
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+  var messagesuccess="Email address copied to clipboard";
+  alert(messagesuccess);
+
+}
